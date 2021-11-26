e710a04bf414a4147db10996aa6d86facbcfbc70
lriccotti <nayy4033@gmail.com>
2021-11-26 16:28:28 -0300

    a

---
+++ Atlas_Sistema.solar/MainForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 23/11/2021
+ * Time: 12:44
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Atlas_Sistema.solar
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pc1 = new System.Windows.Forms.PictureBox();
+			this.pc2 = new System.Windows.Forms.PictureBox();
+			this.pc3 = new System.Windows.Forms.PictureBox();
+			this.pc4 = new System.Windows.Forms.PictureBox();
+			this.pc5 = new System.Windows.Forms.PictureBox();
+			this.pc6 = new System.Windows.Forms.PictureBox();
+			this.pc7 = new System.Windows.Forms.PictureBox();
+			this.pc8 = new System.Windows.Forms.PictureBox();
+			this.pic_central = new System.Windows.Forms.PictureBox();
+			this.lb1 = new System.Windows.Forms.Label();
+			this.lb2 = new System.Windows.Forms.Label();
+			this.lb3 = new System.Windows.Forms.Label();
+			this.lb4 = new System.Windows.Forms.Label();
+			this.lb5 = new System.Windows.Forms.Label();
+			this.lb6 = new System.Windows.Forms.Label();
+			this.lb8 = new System.Windows.Forms.Label();
+			this.lb7 = new System.Windows.Forms.Label();
+			this.lb_nombre = new System.Windows.Forms.Label();
+			this.lb_descripcion = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pc1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_central)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pc1
+			// 
+			this.pc1.Location = new System.Drawing.Point(29, 15);
+			this.pc1.Name = "pc1";
+			this.pc1.Size = new System.Drawing.Size(69, 66);
+			this.pc1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc1.TabIndex = 0;
+			this.pc1.TabStop = false;
+			this.pc1.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc2
+			// 
+			this.pc2.Location = new System.Drawing.Point(29, 110);
+			this.pc2.Name = "pc2";
+			this.pc2.Size = new System.Drawing.Size(69, 66);
+			this.pc2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc2.TabIndex = 1;
+			this.pc2.TabStop = false;
+			this.pc2.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc3
+			// 
+			this.pc3.Location = new System.Drawing.Point(29, 201);
+			this.pc3.Name = "pc3";
+			this.pc3.Size = new System.Drawing.Size(69, 66);
+			this.pc3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc3.TabIndex = 2;
+			this.pc3.TabStop = false;
+			this.pc3.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc4
+			// 
+			this.pc4.Location = new System.Drawing.Point(29, 290);
+			this.pc4.Name = "pc4";
+			this.pc4.Size = new System.Drawing.Size(69, 66);
+			this.pc4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc4.TabIndex = 3;
+			this.pc4.TabStop = false;
+			this.pc4.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc5
+			// 
+			this.pc5.Location = new System.Drawing.Point(604, 15);
+			this.pc5.Name = "pc5";
+			this.pc5.Size = new System.Drawing.Size(69, 66);
+			this.pc5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc5.TabIndex = 4;
+			this.pc5.TabStop = false;
+			this.pc5.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc6
+			// 
+			this.pc6.Location = new System.Drawing.Point(604, 110);
+			this.pc6.Name = "pc6";
+			this.pc6.Size = new System.Drawing.Size(69, 66);
+			this.pc6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc6.TabIndex = 5;
+			this.pc6.TabStop = false;
+			this.pc6.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc7
+			// 
+			this.pc7.Location = new System.Drawing.Point(604, 201);
+			this.pc7.Name = "pc7";
+			this.pc7.Size = new System.Drawing.Size(69, 66);
+			this.pc7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc7.TabIndex = 6;
+			this.pc7.TabStop = false;
+			this.pc7.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pc8
+			// 
+			this.pc8.Location = new System.Drawing.Point(604, 290);
+			this.pc8.Name = "pc8";
+			this.pc8.Size = new System.Drawing.Size(69, 66);
+			this.pc8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pc8.TabIndex = 7;
+			this.pc8.TabStop = false;
+			this.pc8.Click += new System.EventHandler(this.Pc1Click);
+			// 
+			// pic_central
+			// 
+			this.pic_central.Location = new System.Drawing.Point(216, 75);
+			this.pic_central.Name = "pic_central";
+			this.pic_central.Size = new System.Drawing.Size(234, 159);
+			this.pic_central.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pic_central.TabIndex = 8;
+			this.pic_central.TabStop = false;
+			// 
+			// lb1
+			// 
+			this.lb1.Location = new System.Drawing.Point(29, 84);
+			this.lb1.Name = "lb1";
+			this.lb1.Size = new System.Drawing.Size(100, 23);
+			this.lb1.TabIndex = 9;
+			this.lb1.Text = "Mecurio";
+			// 
+			// lb2
+			// 
+			this.lb2.Location = new System.Drawing.Point(29, 179);
+			this.lb2.Name = "lb2";
+			this.lb2.Size = new System.Drawing.Size(100, 23);
+			this.lb2.TabIndex = 10;
+			this.lb2.Text = "Venus";
+			// 
+			// lb3
+			// 
+			this.lb3.Location = new System.Drawing.Point(29, 270);
+			this.lb3.Name = "lb3";
+			this.lb3.Size = new System.Drawing.Size(100, 23);
+			this.lb3.TabIndex = 11;
+			this.lb3.Text = "Tierra";
+			// 
+			// lb4
+			// 
+			this.lb4.Location = new System.Drawing.Point(29, 359);
+			this.lb4.Name = "lb4";
+			this.lb4.Size = new System.Drawing.Size(100, 23);
+			this.lb4.TabIndex = 12;
+			this.lb4.Text = "Marte";
+			this.lb4.Click += new System.EventHandler(this.Label4Click);
+			// 
+			// lb5
+			// 
+			this.lb5.Location = new System.Drawing.Point(573, 84);
+			this.lb5.Name = "lb5";
+			this.lb5.Size = new System.Drawing.Size(100, 23);
+			this.lb5.TabIndex = 13;
+			this.lb5.Text = "Jupiter";
+			// 
+			// lb6
+			// 
+			this.lb6.Location = new System.Drawing.Point(573, 179);
+			this.lb6.Name = "lb6";
+			this.lb6.Size = new System.Drawing.Size(100, 23);
+			this.lb6.TabIndex = 14;
+			this.lb6.Text = "Saturno";
+			// 
+			// lb8
+			// 
+			this.lb8.Location = new System.Drawing.Point(582, 359);
+			this.lb8.Name = "lb8";
+			this.lb8.Size = new System.Drawing.Size(100, 23);
+			this.lb8.TabIndex = 15;
+			this.lb8.Text = "Neptuno";
+			// 
+			// lb7
+			// 
+			this.lb7.Location = new System.Drawing.Point(573, 270);
+			this.lb7.Name = "lb7";
+			this.lb7.Size = new System.Drawing.Size(100, 23);
+			this.lb7.TabIndex = 15;
+			this.lb7.Text = "Urano";
+			// 
+			// lb_nombre
+			// 
+			this.lb_nombre.Location = new System.Drawing.Point(295, 49);
+			this.lb_nombre.Name = "lb_nombre";
+			this.lb_nombre.Size = new System.Drawing.Size(100, 23);
+			this.lb_nombre.TabIndex = 16;
+			this.lb_nombre.Text = "label9";
+			// 
+			// lb_descripcion
+			// 
+			this.lb_descripcion.Location = new System.Drawing.Point(216, 237);
+			this.lb_descripcion.Name = "lb_descripcion";
+			this.lb_descripcion.Size = new System.Drawing.Size(234, 144);
+			this.lb_descripcion.TabIndex = 17;
+			this.lb_descripcion.Text = "label10";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(722, 390);
+			this.Controls.Add(this.lb_descripcion);
+			this.Controls.Add(this.lb_nombre);
+			this.Controls.Add(this.lb7);
+			this.Controls.Add(this.lb8);
+			this.Controls.Add(this.lb6);
+			this.Controls.Add(this.lb5);
+			this.Controls.Add(this.lb4);
+			this.Controls.Add(this.lb3);
+			this.Controls.Add(this.lb2);
+			this.Controls.Add(this.lb1);
+			this.Controls.Add(this.pic_central);
+			this.Controls.Add(this.pc8);
+			this.Controls.Add(this.pc7);
+			this.Controls.Add(this.pc6);
+			this.Controls.Add(this.pc5);
+			this.Controls.Add(this.pc4);
+			this.Controls.Add(this.pc3);
+			this.Controls.Add(this.pc2);
+			this.Controls.Add(this.pc1);
+			this.Name = "MainForm";
+			this.Text = "Atlas_Sistema.solar";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pc1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pc8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_central)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label lb_descripcion;
+		private System.Windows.Forms.Label lb_nombre;
+		private System.Windows.Forms.Label lb7;
+		private System.Windows.Forms.Label lb8;
+		private System.Windows.Forms.Label lb6;
+		private System.Windows.Forms.Label lb5;
+		private System.Windows.Forms.Label lb4;
+		private System.Windows.Forms.Label lb3;
+		private System.Windows.Forms.Label lb2;
+		private System.Windows.Forms.Label lb1;
+		private System.Windows.Forms.PictureBox pic_central;
+		private System.Windows.Forms.PictureBox pc8;
+		private System.Windows.Forms.PictureBox pc7;
+		private System.Windows.Forms.PictureBox pc6;
+		private System.Windows.Forms.PictureBox pc5;
+		private System.Windows.Forms.PictureBox pc4;
+		private System.Windows.Forms.PictureBox pc3;
+		private System.Windows.Forms.PictureBox pc2;
+		private System.Windows.Forms.PictureBox pc1;
+	}
+}
---
+++ Atlas_Sistema.solar/MainForm.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 23/11/2021
+ * Time: 12:44
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Atlas_Sistema.solar
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string[] imagen;
+		string[] nombre;
+		string[] informacion;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+	
+		
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			imagen= new string[8];
+			string dreccion="C:\\Users\\VaCkensi\\Pictures\\atlas\\";
+			
+			imagen[0] =dreccion+"1.jpg";
+			imagen[1] =dreccion+"2.jpg";
+			imagen[2] =dreccion+"3.jpg";
+			imagen[3] =dreccion+"4.jpg";
+			imagen[4] =dreccion+"5.jpg";
+			imagen[5] =dreccion+"6.jpg";
+			imagen[6] =dreccion+"7.jpg";
+			imagen[7] =dreccion+"8.jpg";
+			
+			pc1.Image = Image.FromFile(imagen[0]);
+			pc2.Image = Image.FromFile(imagen[1]);
+			pc3.Image = Image.FromFile(imagen[2]);
+			pc4.Image = Image.FromFile(imagen[3]);
+			pc5.Image = Image.FromFile(imagen[4]);
+			pc6.Image = Image.FromFile(imagen[5]);
+			pc7.Image = Image.FromFile(imagen[6]);
+			pc8.Image = Image.FromFile(imagen[7]);
+			
+			nombre= new string[8];
+			nombre[0]="Mercurio";
+			nombre[1]="Venus";
+			nombre[2]="Tierra";
+			nombre[3]="Marte";
+			nombre[4]="Jupiter";
+			nombre[5]="Saturno";
+			nombre[6]="Urano";
+			nombre[7]="Neptuno";
+		
+			informacion= new string[8];
+			informacion[0]="Mercurio es el planeta más pequeño de nuestro sistema solar. Simplemente, es un poco más grande que la Luna de la Tierra. Es el planeta más cercano al Sol, pero no es realmente el más cálido";
+			informacion[1]="carece de satélites naturales. Recibe su nombre en honor a Venus, la diosa romana del amor. ";
+			informacion[2]="Nuestro hogar, el planeta Tierra, es un planeta terrestre y rocoso. Tiene una superficie sólida y activa, con montañas, valles, cañones, llanuras y mucho más. La Tierra es especial porque es un planeta océano, ya que el agua cubre el 70% de su superficie.";
+			informacion[3]="Marte es un planeta desértico y frío. Es la mitad del tamaño de la Tierra, y también recibe el nombre de planeta rojo'. Es rojo por el hierro oxidado que tiene en el suelo. ";
+			informacion[4]="Júpiter es el planeta más grande del sistema solar y el quinto en orden de lejanía al Sol.​ Es un gigante gaseoso que forma parte de los denominados planetas exteriores. Recibe su nombre del dios romano Júpiter.";
+			informacion[5]="Saturno es el sexto planeta del sistema solar contando desde el Sol, el segundo en tamaño y masa después de Júpiter y el único con un sistema de anillos visible desde la Tierra. ";
+			informacion[6]="Urano es el séptimo planeta del sistema solar, el tercero de mayor tamaño, y el cuarto más masivo. Se llama así en honor de la divinidad griega del cielo Urano, el padre de Crono y el abuelo de Zeus.";
+			informacion[7]="Neptuno es el octavo planeta en distancia respecto al Sol y el más lejano del sistema solar. Forma parte de los denominados planetas exteriores, y dentro de estos, es uno de los gigantes helados, y es el primero que fue descubierto gracias a predicciones matemáticas.";
+			
+			
+			lb_nombre.Text=nombre[0];
+			lb_descripcion.Text= informacion[0];
+			pic_central.Image=pc1.Image;
+		}
+		void Label4Click(object sender, EventArgs e)
+		{
+		}
+		
+		
+		
+		void Pc1Click(object sender, EventArgs e)
+		{
+			PictureBox aux= (PictureBox)sender;
+			pic_central.Image= aux.Image;
+			
+			
+			int i;
+			i = Convert.ToInt32(aux.Tag);
+			pic_central.Image = Image.FromFile(imagen[i]);
+			lb_nombre.Text = nombre[i];
+			lb_descripcion.Text = informacion[i];
+		}
+	}
+}
